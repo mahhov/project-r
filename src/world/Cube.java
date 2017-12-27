22e5e3b7fb43b594bc2948f59449d88773d806b6
m <mahhov1@gmail.com>
2017-12-27 15:49:24 -0800

    element drawing - no color

--- src/world/Cube.java
+++ src/world/Cube.java
@@ -2,6 +2,7 @@ package world;
 
 import org.lwjgl.system.MemoryUtil;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
 import static org.lwjgl.opengl.GL11.*;
@@ -11,14 +12,16 @@ import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
-public class Cube {
-    private float[] vertices, colors;
-    private FloatBuffer verticesBuffer, colorsBuffer;
+class Cube {
+    private float[] vertices;
+    private byte[] elements;
+    private FloatBuffer verticesBuffer;
+    private ByteBuffer elementsBuffer;
     private int vaoId;
 
     Cube(float x, float y, float z) {
         createVertices(x, y, z);
-        createColors();
+        createElements();
         createBuffers();
         createVao();
     }
@@ -27,68 +30,34 @@ public class Cube {
         float left = x - .5f, right = x + .5f, front = y - .5f, back = y + .5f, top = z - .5f, bottom = z + .5f;
 
         vertices = new float[] {
-                left, front, top,
-                left, back, top,
-                left, back, bottom,
-                left, front, top,
-                left, back, bottom,
-                left, front, bottom,
-                right, back, top,
-                right, front, top,
-                right, front, bottom,
-                right, back, top,
-                right, front, bottom,
-                right, back, bottom,
-                left, front, top,
-                left, front, bottom,
-                right, front, bottom,
-                left, front, top,
-                right, front, bottom,
-                right, front, top,
-                right, back, top,
-                right, back, bottom,
-                left, back, bottom,
-                right, back, top,
-                left, back, bottom,
-                left, back, top,
-                right, back, bottom,
-                right, front, bottom,
-                left, front, bottom,
-                right, back, bottom,
-                left, front, bottom,
-                left, back, bottom,
-                left, back, top,
-                left, front, top,
-                right, front, top,
-                left, back, top,
-                right, front, top,
-                right, back, top
+                left, front, top, // 0
+                left, front, bottom, // 1
+                left, back, top, // 2
+                left, back, bottom, // 3
+                right, front, top, // 4
+                right, front, bottom, // 5
+                right, back, top, // 6
+                right, back, bottom, // 7
         };
     }
 
-    private void createColors() {
-        float[][] color = new float[6][];
-        color[0] = new float[] {1, 0, 0};
-        color[1] = new float[] {0, 1, 0};
-        color[2] = new float[] {1, 0, 1};
-        color[3] = new float[] {1, 1, 0};
-        color[4] = new float[] {0, 0, 1};
-        color[5] = new float[] {1, 1, 1};
-
-        colors = new float[vertices.length];
-        for (int side = 0; side < 6; side++) {
-            for (int point = 0; point < 6; point++)
-                for (int rgb = 0; rgb < 3; rgb++)
-                    colors[side * 18 + point * 3 + rgb] = color[side][rgb];
-        }
+    private void createElements() {
+        elements = new byte[] {
+                0, 2, 3, 0, 3, 1, // left
+                4, 7, 6, 4, 5, 7, // right
+                0, 1, 5, 0, 5, 4, // front
+                2, 3, 7, 2, 7, 6, // back
+                0, 4, 6, 0, 6, 2, // top
+                1, 5, 7, 1, 7, 3, // bottom
+        };
     }
 
     private void createBuffers() {
         verticesBuffer = MemoryUtil.memAllocFloat(vertices.length);
         verticesBuffer.put(vertices).flip();
 
-        colorsBuffer = MemoryUtil.memAllocFloat(colors.length);
-        colorsBuffer.put(colors).flip();
+        elementsBuffer = MemoryUtil.memAlloc(elements.length);
+        elementsBuffer.put(elements).flip();
     }
 
     private void createVao() {
@@ -102,18 +71,14 @@ public class Cube {
         glEnableVertexAttribArray(0);
         MemoryUtil.memFree(verticesBuffer);
 
-        int colorsVboId = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, colorsVboId);
-        glBufferData(GL_ARRAY_BUFFER, colorsBuffer, GL_STATIC_DRAW);
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
-        glEnableVertexAttribArray(1);
-        MemoryUtil.memFree(colorsBuffer);
+        int elementsVboId = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementsVboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementsBuffer, GL_STATIC_DRAW);
+        MemoryUtil.memFree(elementsBuffer);
     }
 
-    public void draw() {
-        for (int i = 0; i < 6; i++) {
-            glBindVertexArray(vaoId);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+    void draw() {
+        glBindVertexArray(vaoId);
+        glDrawElements(GL_TRIANGLES, elements.length, GL_UNSIGNED_BYTE, 0);
     }
 }
