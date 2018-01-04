e0e71bb5b25b7b2625f87f60cf3ae640f07b30fc
m <mahhov1@gmail.com>
2018-01-03 19:38:17 -0800

    adjust (reduce) stamina cost for jump and air jump

--- src/character/Stamina.java
+++ src/character/Stamina.java
@@ -3,7 +3,7 @@ package character;
 import util.MathNumbers;
 
 class Stamina {
-    static final float JUMP = 5, AIR_JUMP = 15, JET = .11f, GLIDE = .07f, BOOST = 5;
+    static final float JUMP = 3, AIR_JUMP = 9, JET = .11f, GLIDE = .07f, BOOST = 5;
 
     private float max, current, regen;
     private float maxReserve, currentReserve, regenReserve;
