9037ee55f9245b546f059bf8a9fcd63d78d2ee7f
m <mahhov1@gmail.com>
2018-01-06 16:44:21 -0800

    Timer add optional parameter for minTime required in order to display message

--- src/util/Timer.java
+++ src/util/Timer.java
@@ -18,4 +18,15 @@ public class Timer {
         beginTime = System.nanoTime();
         return difTime;
     }
+    
+    public static long time(String message, long minTimeMs) {
+        long endTime = System.nanoTime();
+        long difTime = endTime - beginTime;
+        if (message != null && difTime / NANOSECONDS_IN__MILLISECOND >= minTimeMs) {
+            String displayTime = difTime < 4 * NANOSECONDS_IN__MILLISECOND ? (int) (1000 * difTime / NANOSECONDS_IN__MILLISECOND) / 1000. + "ms" : difTime / NANOSECONDS_IN__MILLISECOND + "ms";
+            System.out.println(message + " (" + displayTime + ")");
+        }
+        beginTime = System.nanoTime();
+        return difTime;
+    }
 }
