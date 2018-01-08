5f02043513bfbd733b4328e2168f80ee8b679779
m <mahhov1@gmail.com>
2018-01-08 07:57:54 -0800

    refactored Timer duplicate code for printing times and provided public method for printing times

--- src/util/Timer.java
+++ src/util/Timer.java
@@ -11,22 +11,23 @@ public class Timer {
     public static long time(String message) {
         long endTime = System.nanoTime();
         long difTime = endTime - beginTime;
-        if (message != null) {
-            String displayTime = difTime < 4 * NANOSECONDS_IN__MILLISECOND ? (int) (1000 * difTime / NANOSECONDS_IN__MILLISECOND) / 1000. + "ms" : difTime / NANOSECONDS_IN__MILLISECOND + "ms";
-            System.out.println(message + " (" + displayTime + ")");
-        }
+        if (message != null)
+            printTime(message, difTime);
         beginTime = System.nanoTime();
         return difTime;
     }
-    
+
     public static long time(String message, long minTimeMs) {
         long endTime = System.nanoTime();
         long difTime = endTime - beginTime;
-        if (message != null && difTime / NANOSECONDS_IN__MILLISECOND >= minTimeMs) {
-            String displayTime = difTime < 4 * NANOSECONDS_IN__MILLISECOND ? (int) (1000 * difTime / NANOSECONDS_IN__MILLISECOND) / 1000. + "ms" : difTime / NANOSECONDS_IN__MILLISECOND + "ms";
-            System.out.println(message + " (" + displayTime + ")");
-        }
+        if (message != null && difTime / NANOSECONDS_IN__MILLISECOND >= minTimeMs)
+            printTime(message, difTime);
         beginTime = System.nanoTime();
         return difTime;
+    }
+
+    public static void printTime(String message, long time) {
+        String displayTime = time < 4 * NANOSECONDS_IN__MILLISECOND ? (int) (1000 * time / NANOSECONDS_IN__MILLISECOND) / 1000. + "ms" : time / NANOSECONDS_IN__MILLISECOND + "ms";
+        System.out.println(message + " (" + displayTime + ")");
     }
 }
