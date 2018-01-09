f3b4750331be5af4b4d05ad94384fb4801807e9d
m <mahhov1@gmail.com>
2018-01-09 10:14:51 -0800

    expand timer to allow passing no message, and to allow tracking total time

--- src/util/Timer.java
+++ src/util/Timer.java
@@ -3,28 +3,34 @@ package util;
 public class Timer {
     private static final int COUNT = 10;
     private static final long NANOSECONDS_IN__MILLISECOND = 1000000L;
-    private static long beginTime[] = new long[COUNT];
+    private static long beginTime[] = new long[COUNT], totalTime[] = new long[COUNT];
 
     public static void restart(int i) {
         beginTime[i] = System.nanoTime();
     }
 
+    public static long time(int i) {
+        return time(i, null, 0);
+    }
+
     public static long time(int i, String message) {
-        long endTime = System.nanoTime();
-        long difTime = endTime - beginTime[i];
-        if (message != null)
-            printTime(message, difTime);
-        beginTime[i] = System.nanoTime();
-        return difTime;
+        return time(i, message, 0);
     }
 
     public static long time(int i, String message, long minTimeMs) {
         long endTime = System.nanoTime();
         long difTime = endTime - beginTime[i];
+        totalTime[i] += difTime;
         if (message != null && difTime / NANOSECONDS_IN__MILLISECOND >= minTimeMs)
             printTime(message, difTime);
         beginTime[i] = System.nanoTime();
         return difTime;
+    }
+
+    public static long totalTime(int i, String message) {
+        if (message != null)
+            printTime(message, totalTime[i]);
+        return totalTime[i];
     }
 
     public static void printTime(String message, long time) {
