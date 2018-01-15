5cb96e017a9a1c83b33fd476e7f3b779a62c543c
m <mahhov1@gmail.com>
2018-01-15 15:40:55 -0800

    wire in point allocation to stat changes for health & stamina

--- src/character/Experience.java
+++ src/character/Experience.java
@@ -6,8 +6,8 @@ public class Experience {
     private int maxExperience, level, experience;
 
     public enum Skill {
-        LIFE_REGEN("Life & Shield Regen"), LIFE_RAW("Maximum Life & Shield"), LIFE("Maximum Life & Life Regen"), SHIELD("Maximum Shield & Shield Regen"),
-        STAMINA_REGEN("Stamina & Reserve Regen"), STAMINA_RAW("Maximum Stamina & Reserve"), STAMINA("Maximum Stamina & Stamina Regen"), RESERVE("Maximum Reserve & Reserve Regen"),
+        HEALTH_REGEN("Life & Shield Regen"), HEALTH_RAW("Maximum Life & Shield"), HEALTH_LIFE("Maximum Life & Life Regen"), HEALTH_SHIELD("Maximum Shield & Shield Regen"),
+        STAMINA_REGEN("Stamina & Reserve Regen"), STAMINA_RAW("Maximum Stamina & Reserve"), STAMINA_STAMINA("Maximum Stamina & Stamina Regen"), STAMINA_RESERVE("Maximum Reserve & Reserve Regen"),
         RUN_AIR_ACC("Movement Speed"), JUMP_ACC("Jump Power"), JET_ACC("Jet Speed"), BOOST_GLIDE_ACC("Boost Power");
 
         final String name;
@@ -69,6 +69,15 @@ public class Experience {
         stats.jetAcc.update(points[Skill.JET_ACC.value]);
         stats.boostAcc.update(points[Skill.BOOST_GLIDE_ACC.value]);
         stats.glideAcc.update(points[Skill.BOOST_GLIDE_ACC.value]);
+
+        stats.stamina.update(points[Skill.STAMINA_RAW.value] + points[Skill.STAMINA_STAMINA.value]);
+        stats.staminaRegen.update(points[Skill.STAMINA_REGEN.value] + points[Skill.STAMINA_STAMINA.value]);
+        stats.staminaReserve.update(points[Skill.STAMINA_RAW.value] + points[Skill.STAMINA_RESERVE.value]);
+        stats.staminaReserveRegen.update(points[Skill.STAMINA_REGEN.value] + points[Skill.STAMINA_RESERVE.value]);
+        stats.life.update(points[Skill.HEALTH_RAW.value] + points[Skill.HEALTH_LIFE.value]);
+        stats.lifeRegen.update(points[Skill.HEALTH_REGEN.value] + points[Skill.HEALTH_LIFE.value]);
+        stats.shield.update(points[Skill.HEALTH_RAW.value] + points[Skill.HEALTH_SHIELD.value]);
+        stats.shieldRegen.update(points[Skill.HEALTH_REGEN.value] + points[Skill.HEALTH_SHIELD.value]);
     }
 
     int level() {
