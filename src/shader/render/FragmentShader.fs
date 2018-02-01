b6049346a02d5ea2e1a2bb5e8185cc6cfdd46647
m <mahhov1@gmail.com>
2018-02-01 14:42:04 -0800

    slight clean up frag shader renderer

--- src/shader/render/FragmentShader.fs
+++ src/shader/render/FragmentShader.fs
@@ -8,19 +8,18 @@ in vec3 vertexColor;
 
 out vec3 fragColor;
 
-const float ambientFactor = .2, diffusePower = .8;
+const float ambientFactor = .2, diffusePower = .8, specularPower = 15;
 const vec3 lightPosition = vec3(128, 250, -128);
 const vec3 cameraForward = vec3(0, 0, 1);
-const float specularPower = 10;
 
 float calcDiffuseFactor(vec3 lightDirection) {
-    return max(dot(mVertexNormal, lightDirection), 0.0);
+    return max(dot(mVertexNormal, lightDirection), 0);
 }
 
 float calcSpecularFactor(vec3 lightDirection) {
-    vec3 reflected = normalize(reflect(-lightDirection, mVertexNormal));
-    vec3 vReflected = (view * vec4(reflected, 0.0)).xyz;
-    float factor = max(dot(vReflected, cameraForward), 0.0);
+    vec3 reflected = reflect(-lightDirection, mVertexNormal); // mVertexNormal * 2 * dot(mVertexNormal, lightDirection) - lightDirection
+    vec3 vReflected = (view * vec4(reflected, 0)).xyz;
+    float factor = max(dot(vReflected, cameraForward), 0);
     return pow(factor, specularPower);
 }
 
@@ -28,7 +27,7 @@ void main() {
     vec3 lightDirection = normalize(lightPosition - mVertexPosition);
     float diffuseFactor = calcDiffuseFactor(lightDirection);
     float specularFactor = calcSpecularFactor(lightDirection);
-    float brightness = min(specularFactor + diffuseFactor * diffusePower + ambientFactor, 1);
+    float brightness = min(ambientFactor + diffuseFactor * diffusePower + specularFactor, 1);
     // brightness = floor(brightness * 5) / 5;
     fragColor = vertexColor * brightness;
 }
