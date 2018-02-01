37747b86fdcfa0ff77d5f64792084de2705b117f
m <mahhov1@gmail.com>
2018-02-01 11:59:11 -0800

    stop retaliation after time with distance apart

--- src/character/monster/motion/RetaliateMotion.java
+++ src/character/monster/motion/RetaliateMotion.java
@@ -4,9 +4,10 @@ import util.MathNumbers;
 import util.MathRandom;
 
 public class RetaliateMotion extends MonsterMotion {
-    private static final int WANDER_TIME = 100;
+    private static final int RETALIATE_TIME = 300, WANDER_TIME = 100;
     private static final float WANDER_PROB = .7f;
     private static final float RETALIATE_DISTANCE_SPEED_MULT = .2f;
+    private static final float RETALIATE_FORGET_DISTANCE_SQR = 100;
 
     private Timer timer;
     private boolean damageTaken;
@@ -26,25 +27,28 @@ public class RetaliateMotion extends MonsterMotion {
         timer.update();
 
         if (distanceSqr < hostilitySightDistanceSqr || (distanceSqr < hostilityDangerDistanceSqr && human.isDangerous()) || damageTaken) {
+            timer.reset(RETALIATE_TIME);
             retaliate = true;
             damageTaken = false;
         }
 
-        if (retaliate) {
-            float flatDistanceSqr = MathNumbers.magnitudeSqr(dx, dy);
-            run(dx, dy, MathNumbers.min(hostilitySpeed, flatDistanceSqr * RETALIATE_DISTANCE_SPEED_MULT));
-
-        } else if (timer.done()) {
+        if (timer.done()) {
             timer.reset(WANDER_TIME);
+            retaliate = false;
             if (MathRandom.random(WANDER_PROB))
                 wander();
             else
                 stand();
+
+        } else if (retaliate) {
+            float flatDistanceSqr = MathNumbers.magnitudeSqr(dx, dy);
+            run(dx, dy, MathNumbers.min(hostilitySpeed, flatDistanceSqr * RETALIATE_DISTANCE_SPEED_MULT));
+            if (flatDistanceSqr < RETALIATE_FORGET_DISTANCE_SQR)
+                timer.reset(RETALIATE_TIME);
         }
 
         jet();
     }
-    // todo stop retaliation after time with no damaged human nor monster
 
     @Override
     public void damageTaken() {
