807024b20c662e9ce7d054fbe5717d4884638999
m <mahhov1@gmail.com>
2018-02-01 10:40:36 -0800

    basic retaliation motion

--- src/character/monster/motion/RetaliateMotion.java
+++ src/character/monster/motion/RetaliateMotion.java
@@ -4,31 +4,36 @@ import util.MathNumbers;
 import util.MathRandom;
 
 public class RetaliateMotion extends MonsterMotion {
-    private static final int RUNAWAY_TIME = 300, WANDER_TIME = 100;
+    private static final int WANDER_TIME = 100;
     private static final float WANDER_PROB = .7f;
 
-    private Timer runawayTimer;
+    private Timer timer;
+    private boolean damageTaken;
+    private boolean retaliate;
 
     public RetaliateMotion() {
         super();
-        runawayTimer = new Timer();
+        timer = new Timer();
     }
 
     public void update() {
-        float dx = monster.getX() - human.getX();
-        float dy = monster.getY() - human.getY();
-        float dz = monster.getZ() - human.getZ();
+        float dx = human.getX() - monster.getX();
+        float dy = human.getY() - monster.getY();
+        float dz = human.getZ() - monster.getZ();
         float distanceSqr = MathNumbers.magnitudeSqr(dx, dy, dz);
 
-        runawayTimer.update();
+        timer.update();
 
-        if (distanceSqr < hostilitySightDistanceSqr || (distanceSqr < hostilityDangerDistanceSqr && human.isDangerous())) {
-            runawayTimer.reset(RUNAWAY_TIME);
-            run(dx, dy, hostilitySpeed);
+        if (distanceSqr < hostilitySightDistanceSqr || (distanceSqr < hostilityDangerDistanceSqr && human.isDangerous()) || damageTaken) {
+            retaliate = true;
+            damageTaken = false;
         }
 
-        if (runawayTimer.done()) {
-            runawayTimer.reset(WANDER_TIME);
+        if (retaliate) {
+            run(dx, dy, hostilitySpeed);
+
+        } else if (timer.done()) {
+            timer.reset(WANDER_TIME);
             if (MathRandom.random(WANDER_PROB))
                 wander();
             else
@@ -36,7 +41,12 @@ public class RetaliateMotion extends MonsterMotion {
         }
 
         jet();
+    } 
+    // todo stop retaliation after time with no damaged human nor monster
+    // todo dont overrun when close to human 
 
-        // todo make retaliate instead of runaway
+    @Override
+    public void damageTaken() {
+        damageTaken = true;
     }
 }
--- src/character/monster/motion/RunawayMotion.java
+++ src/character/monster/motion/RunawayMotion.java
@@ -7,30 +7,30 @@ public class RunawayMotion extends MonsterMotion {
     private static final int RUNAWAY_TIME = 300, WANDER_TIME = 100;
     private static final float WANDER_PROB = .7f;
 
-    private Timer runawayTimer;
+    private Timer timer;
     private boolean damageTaken;
 
     public RunawayMotion() {
         super();
-        runawayTimer = new Timer();
+        timer = new Timer();
     }
 
     public void update() {
-        float dx = monster.getX() - human.getX();
-        float dy = monster.getY() - human.getY();
-        float dz = monster.getZ() - human.getZ();
+        float dx = human.getX() - monster.getX();
+        float dy = human.getY() - monster.getY();
+        float dz = human.getZ() - monster.getZ();
         float distanceSqr = MathNumbers.magnitudeSqr(dx, dy, dz);
 
-        runawayTimer.update();
+        timer.update();
 
         if (distanceSqr < hostilitySightDistanceSqr || (distanceSqr < hostilityDangerDistanceSqr && human.isDangerous()) || damageTaken) {
-            runawayTimer.reset(RUNAWAY_TIME);
-            run(dx, dy, hostilitySpeed);
+            timer.reset(RUNAWAY_TIME);
+            run(-dx, -dy, hostilitySpeed);
             damageTaken = false;
         }
 
-        if (runawayTimer.done()) {
-            runawayTimer.reset(WANDER_TIME);
+        if (timer.done()) {
+            timer.reset(WANDER_TIME);
             if (MathRandom.random(WANDER_PROB))
                 wander();
             else
