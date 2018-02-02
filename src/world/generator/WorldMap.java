1eb5f072b70e335b9e5ea577546c17bffcfb0e3d
m <mahhov1@gmail.com>
2018-02-02 09:26:14 -0800

    improve terrain generation to weigh environment noise more than height noise

--- src/world/generator/WorldMap.java
+++ src/world/generator/WorldMap.java
@@ -3,9 +3,13 @@ package world.generator;
 import util.MathNumbers;
 
 public class WorldMap {
+    private static final int MAX_TERRAIN = 8;
+    private static final int MIN_HEIGHT = 30, DELTA_HEIGHT = 140, WEIGHT_HEIGHT = 1;
+    private static final int MIN_ENVIRONMENT = 20, DELTA_ENVIRONMENT = 60, WEIGHT_ENVIRONMENT = 5;
+
     public byte[][][] map;
-    public int[][] heightMap;
-    public byte[][] environment;
+    private int[][] heightMap;
+    private byte[][] environment;
 
     WorldMap(byte[][][] map, int[][] heightMap, byte[][] environment) {
         this.map = map;
@@ -14,6 +18,8 @@ public class WorldMap {
     }
 
     public int getTerrain(int x, int y) {
-        return MathNumbers.minMax((heightMap[x][y] + environment[x][y] - 80) / 10, 0, 8); // todo make constants
+        int sum = (heightMap[x][y] - MIN_HEIGHT) * WEIGHT_HEIGHT + (environment[x][y] - MIN_ENVIRONMENT) * WEIGHT_ENVIRONMENT;
+        int delta = DELTA_HEIGHT * WEIGHT_HEIGHT + DELTA_ENVIRONMENT * WEIGHT_ENVIRONMENT;
+        return MathNumbers.minMax(sum * MAX_TERRAIN / delta, 0, MAX_TERRAIN);
     }
 }
