44fa6c85869f24a6a7c07ae19ac198998baff24e
m <mahhov1@gmail.com>
2018-02-24 17:49:05 -0800

    ModelGenerator

---
+++ src/modelviewer/ModelGenerator.java
@@ -0,0 +1,19 @@
+package modelviewer;
+
+import model.ViewModel;
+import util.Writer;
+
+import java.io.IOException;
+
+public class ModelGenerator {
+    public static void main(String[] args) {
+        for (ViewModelProvider.ViewModelType viewModelType : ViewModelProvider.ViewModelType.values()) {
+            ViewModel model = ViewModelProvider.getViewModel(viewModelType, null);
+            try {
+                Writer.getWriteStream(viewModelType.file).writeObject(model.getModelData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
