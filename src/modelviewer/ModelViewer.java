55b2a1826fd558573f5549aa5940ab29e0dafddf
m <mahhov1@gmail.com>
2018-02-26 08:40:29 -0800

    new model viewer wip

--- src/modelviewer/ModelViewer.java
+++ src/modelviewer/ModelViewer.java
@@ -1,139 +1,90 @@
-//package modelviewer;
-//
-//import camera.Camera;
-//import camera.FreeCameraFollow;
-//import control.Controls;
-//import control.KeyButton;
-//import control.MouseButton;
-//import engine.Engine;
-//import engine.EngineRunnable;
-//import model.ModelData;
-//import model.ViewModel;
-//import shader.ShaderManager;
-//import shape.CubeInstancedFaces;
-//import ui.UiDrawerModelViewer;
-//import util.Writer;
-//
-//import java.io.IOException;
-//
-//public class ModelViewer implements EngineRunnable {
-//    private final ViewModelProvider.ViewModelType VIEW_MODEL_TYPE = ViewModelProvider.ViewModelType.BIRD;
-//
-//    private Controls controls;
-//
-//    private CubeInstancedFaces cubeInstancedFaces;
-//    private FreeCameraFollow follow;
-//    private Camera camera;
-//    private ViewModel viewModel;
-//    private Selector selector;
-//    private UiDrawerModelViewer uiDrawer;
-//
-//    @Override
-//    public void init(Controls controls) {
-//        this.controls = controls;
-//        controls.mousePosControl.unlock();
-//
-//        ShaderManager.setRenderShader();
-//
-//        camera = new Camera(ShaderManager.getRenderShaderProgramId(), 20, 0);
-//        follow = new FreeCameraFollow();
-//        camera.setFollow(follow);
-//
-//        createViewModel();
-//
-//        selector = new Selector();
-//        uiDrawer = new UiDrawerModelViewer(selector, controls.keyControl, controls.mousePosControl, controls.mouseButtonControl);
-//    }
-//
-//    @Override
-//    public void printHelp() {
-//        System.out.println("W A S D move / scale horizontally");
-//        System.out.println("Q E rotate");
-//        System.out.println("SPACE SHIFT move / scale vertically");
-//        System.out.println("ENTER selector menu");
-//        System.out.println("N prev segment");
-//        System.out.println("M next segment");
-//        System.out.println("B new");
-//        System.out.println("C load");
-//        System.out.println("V store");
-//        System.out.println("DRAG MOUSE SECONDARY rotate camera");
-//        System.out.println("DRAG MOUSE PRIMARY translate / rotate / scale segment");
-//        System.out.println("Z X zoom camera");
-//        System.out.println("R F move camera vertically");
-//        System.out.println("SCROLL WHEEL change selected segment");
-//        System.out.println("SCROLL WHEEL HORIZONTAL change selected tool");
-//    }
-//
-//    private void storeViewModel() {
-//        try {
-//            Writer.getWriteStream(VIEW_MODEL_TYPE.file).writeObject(viewModel.getModelData());
-//            System.out.println("stored view model " + VIEW_MODEL_TYPE.file);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private void loadViewModel() {
-//        cubeInstancedFaces = new CubeInstancedFaces();
-//        viewModel = new ViewModel(ModelData.readModelData(VIEW_MODEL_TYPE.file), cubeInstancedFaces);
-//        System.out.println("loaded view model " + VIEW_MODEL_TYPE.file);
-//    }
-//
-//    private void createViewModel() {
-//        cubeInstancedFaces = new CubeInstancedFaces();
-//        viewModel = ViewModelProvider.getViewModel(VIEW_MODEL_TYPE, cubeInstancedFaces);
-//    }
-//
-//    @Override
-//    public void loop() {
-//        if (controls.keyControl.isKeyPressed(KeyButton.KEY_B))
-//            createViewModel();
-//        if (controls.keyControl.isKeyPressed(KeyButton.KEY_C))
-//            loadViewModel();
-//        if (controls.keyControl.isKeyPressed(KeyButton.KEY_V))
-//            storeViewModel();
-//
-//        ShaderManager.setRenderShader();
-//
-//        uiDrawer.update();
-//
-//        if (controls.mouseButtonControl.isMousePressed(MouseButton.PRIMARY) || controls.mouseButtonControl.isMousePressed(MouseButton.SECONDARY))
-//            controls.mousePosControl.lock();
-//        else if (controls.mouseButtonControl.isMouseReleased(MouseButton.PRIMARY) || controls.mouseButtonControl.isMouseReleased(MouseButton.SECONDARY))
-//            controls.mousePosControl.unlock();
-//        if (controls.mouseButtonControl.isMouseDown(MouseButton.PRIMARY))
-//            viewModel.update(selector.getSelectedTool(), viewModel.normalizeControl(controls.mousePosControl, controls.keyControl.isKeyDown(KeyButton.KEY_SHIFT)));
-//        if (controls.mouseButtonControl.isMouseDown(MouseButton.SECONDARY))
-//            follow.update(controls.mousePosControl);
-//
-//        camera.update(controls.keyControl);
-//        selector.update(controls.keyControl, controls.mouseScrollControl);
-//        viewModel.update(selector.getSelectedTool(), viewModel.normalizeControl(controls.keyControl));
-//        viewModel.updtaeSelectedSegment(selector.getSelectedSegmentDelta());
-//
-//        cubeInstancedFaces.reset();
-//        viewModel.draw();
-//        cubeInstancedFaces.doneAdding();
-//        cubeInstancedFaces.draw();
-//
-//        ShaderManager.setUiShader();
-//        uiDrawer.draw();
-//        ShaderManager.setTextShader();
-//        uiDrawer.drawText();
-//
-//        controls.mouseButtonControl.next();
-//    }
-//
-//    @Override
-//    public void updateFps(int fps) {
-//        uiDrawer.updateFps(fps);
-//    }
-//
-//    @Override
-//    public void shutDown() {
-//    }
-//
-//    public static void main(String[] args) {
-//        new Engine(new ModelViewer());
-//    }
-//}
+package modelviewer;
+
+import camera.Camera;
+import camera.FreeCameraFollow;
+import control.Controls;
+import control.MouseButton;
+import engine.Engine;
+import engine.EngineRunnable;
+import model.Model;
+import model.ModelData;
+import shader.ShaderManager;
+import shape.CubeInstancedFaces;
+
+public class ModelViewer implements EngineRunnable {
+    private final ModelData.ModelType MODAL_TYPE = ModelData.ModelType.BIRD;
+
+    private Controls controls;
+
+    private FreeCameraFollow follow;
+    private Camera camera;
+    private CubeInstancedFaces cubeInstancedFaces;
+    private Model model;
+
+    @Override
+    public void init(Controls controls) {
+        this.controls = controls;
+        controls.mousePosControl.unlock();
+
+        ShaderManager.setRenderShader();
+
+        follow = new FreeCameraFollow();
+        camera = new Camera(ShaderManager.getRenderShaderProgramId(), 20, 0);
+        camera.setFollow(follow);
+        cubeInstancedFaces = new CubeInstancedFaces();
+        model = new Model(MODAL_TYPE.modelData, cubeInstancedFaces);
+    }
+
+    @Override
+    public void printHelp() {
+        System.out.println("TODO : REPLACE THIS HELP");
+        System.out.println("W A S D move / scale horizontally");
+        System.out.println("Q E rotate");
+        System.out.println("SPACE SHIFT move / scale vertically");
+        System.out.println("ENTER selector menu");
+        System.out.println("N prev segment");
+        System.out.println("M next segment");
+        System.out.println("B new");
+        System.out.println("C load");
+        System.out.println("V store");
+        System.out.println("DRAG MOUSE SECONDARY rotate camera");
+        System.out.println("DRAG MOUSE PRIMARY translate / rotate / scale segment");
+        System.out.println("Z X zoom camera");
+        System.out.println("R F move camera vertically");
+        System.out.println("SCROLL WHEEL change selected segment");
+        System.out.println("SCROLL WHEEL HORIZONTAL change selected tool");
+    }
+
+    @Override
+    public void loop() {
+        if (controls.mouseButtonControl.isMousePressed(MouseButton.SECONDARY))
+            controls.mousePosControl.lock();
+        else if (controls.mouseButtonControl.isMouseReleased(MouseButton.SECONDARY))
+            controls.mousePosControl.unlock();
+        //        if (controls.mouseButtonControl.isMouseDown(MouseButton.PRIMARY))
+        //            viewModel.update(selector.getSelectedTool(), viewModel.normalizeControl(controls.mousePosControl, controls.keyControl.isKeyDown(KeyButton.KEY_SHIFT)));
+        if (controls.mouseButtonControl.isMouseDown(MouseButton.SECONDARY))
+            follow.update(controls.mousePosControl);
+
+        camera.update(controls.keyControl);
+
+        cubeInstancedFaces.reset();
+        model.draw();
+        cubeInstancedFaces.doneAdding();
+        cubeInstancedFaces.draw();
+
+        controls.mouseButtonControl.next();
+    }
+
+    @Override
+    public void updateFps(int fps) {
+    }
+
+    @Override
+    public void shutDown() {
+    }
+
+    public static void main(String[] args) {
+        new Engine(new ModelViewer());
+    }
+}
